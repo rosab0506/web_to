rosab0506/web_to

commit f3c44f4c2ba6776d4e36dfe0c66894561fda3586

Move "Information" page common cleaning logic to Parser class

plugin/js/parsers/AnythingNovelParser.js
@@ -36,13 +36,12 @@ class AnythingNovelParser extends Parser{
     }
 
     getInformationEpubItemChildNodes(dom) {
-        let node = dom.querySelector("div#content div:not(.clearfix)");
-        if (node != null) {
-            node = node.cloneNode(true);
-            let strip = [...node.querySelectorAll("img")]
-                .map(e => e.parentElement);
-            util.removeElements(strip);
-        }
-        return [node];
+        return [...dom.querySelector("div#content div:not(.clearfix)")];
+    }
+
+    cleanInformationNode(node) {
+        let strip = [...node.querySelectorAll("img")]
+            .map(e => e.parentElement);
+        util.removeElements(strip);
     }
 }

plugin/js/parsers/FanFictionParser.js
@@ -57,13 +57,10 @@ class FanFictionParser extends Parser {
     }
 
     getInformationEpubItemChildNodes(dom) {
-        let nodes = [];
-        let node = dom.querySelector("div#profile_top");
-        if (node != null) {
-            nodes.push(node);
-            let toRemove = [...node.querySelectorAll("button, span[title]")];
-            util.removeElements(toRemove);
-        }
-        return nodes;
+        return [...dom.querySelector("div#profile_top")];
+    }
+
+    cleanInformationNode(node) {
+        util.removeChildElementsMatchingCss(node, "button, span[title]");
     }
 }

plugin/js/Parser.js
@@ -282,13 +282,32 @@ class Parser {
         urlElement.appendChild(bold);
         urlElement.appendChild(document.createTextNode(this.state.chapterListUrl));
         div.appendChild(urlElement);
-        let childNodes = [div].concat(this.getInformationEpubItemChildNodes(dom));
+        let infoDiv = document.createElement("div");
+        this.populateInfoDiv(infoDiv, dom);    
+        let childNodes = [div, infoDiv];
         let chapter = {
             sourceUrl: this.state.chapterListUrl,
             title: titleText,
             newArch: null
         };
         return new ChapterEpubItem(chapter, {childNodes: childNodes}, 0);
+    }
+
+    populateInfoDiv(infoDiv, dom) {
+        let sanitize = new Sanitize();
+        for(let n of this.getInformationEpubItemChildNodes(dom)) {
+            let clone = n.cloneNode(true);
+            this.cleanInformationNode(clone);
+            if (clone != null) {
+                infoDiv.appendChild(sanitize.clean(clone));
+            }
+        }
+        // this "page" doesn't go through image collector, so strip images
+        util.removeChildElementsMatchingCss(infoDiv, "img");
+    }
+
+    cleanInformationNode(node) {     // eslint-disable-line no-unused-vars
+        // do nothing, derived class overrides as required
     }
 
     // called when plugin has obtained the first web page

plugin/js/parsers/ComrademaoParser.js
@@ -81,9 +81,7 @@ class ComrademaoParser extends Parser{
     };
 
     removeUnwantedElementsFromContentElement(element) {
-        for(let button of element.querySelectorAll("button")) {
-            button.remove();
-        }
+        util.removeChildElementsMatchingCss(element, "button");
         super.removeUnwantedElementsFromContentElement(element);
     }
 
@@ -115,12 +113,6 @@ class ComrademaoParser extends Parser{
     }
 
     getInformationEpubItemChildNodes(dom) {
-        let infoNodes = [...dom.querySelectorAll("div.page-title-product_2 div.wrap-content, div.info-single-product")];
-        for(let e of infoNodes) {
-            for(let i of e.querySelectorAll("img")) {
-                i.remove();
-            }
-        }
-        return infoNodes;
+        return [...dom.querySelectorAll("div.page-title-product_2 div.wrap-content, div.info-single-product")];
     }
 }

plugin/js/parsers/LightNovelBastionParser.js
@@ -39,7 +39,6 @@ class LightNovelBastionParser extends Parser {
     }
 
     getInformationEpubItemChildNodes(dom) {
-        let node = dom.querySelector("article.container header");
-        return (node == null) ? [] : [ node ];
+        return [...dom.querySelectorAll("article.container header")];
     }
 }

plugin/js/parsers/LightNovelsTranslationsParser.js
@@ -45,11 +45,6 @@ class LightNovelsTranslationsParser extends WordpressBaseParser{
     }
 
     getInformationEpubItemChildNodes(dom) {
-        let nodes = [...dom.querySelectorAll("div.entry-content > p")]
-            .map(n => n.cloneNode(true));
-        for(let n of nodes) {
-            util.removeElements(n.querySelectorAll("img"));
-        }
-        return nodes;
+        return [...dom.querySelectorAll("div.entry-content > p")];
     }
 }

plugin/js/parsers/NovelAllParser.js
@@ -45,18 +45,10 @@ class NovelAllParser extends Parser{
     }
 
     getInformationEpubItemChildNodes(dom) {
-        let nodes = [];
-        let summary = dom.querySelectorAll("div.manga-detailtop, div.manga-detailmiddle");
-        for(let node of summary) {
-            let clone = node.cloneNode(true);
-            this.cleanInformationNode(clone);
-            nodes.push(clone);
-        }
-        return nodes;
+        return  [...dom.querySelectorAll("div.manga-detailtop, div.manga-detailmiddle")];
     }
 
     cleanInformationNode(node) {
-        let toRemove = [...node.querySelectorAll("img, script")];
-        util.removeElements(toRemove);
+        util.removeChildElementsMatchingCss(node, "script");
     }
 }

plugin/js/parsers/NovelOnlineFreeParser.js
@@ -51,18 +51,10 @@ class NovelOnlineFreeParser extends Parser{
     }
 
     getInformationEpubItemChildNodes(dom) {
-        let nodes = [];
-        let summary = dom.querySelectorAll("div#noidungm, ul.truyen_info_right");
-        for(let node of summary) {
-            let clone = node.cloneNode(true);
-            this.cleanInformationNode(clone);
-            nodes.push(clone);
-        }
-        return nodes;
+        return [...dom.querySelectorAll("div#noidungm, ul.truyen_info_right")];
     }
 
     cleanInformationNode(node) {
-        let toRemove = [...node.querySelectorAll("span.rate_star, .fb_iframe_widget, div.google, button, img, script")];
-        util.removeElements(toRemove);
+        util.removeChildElementsMatchingCss(node, "span.rate_star, .fb_iframe_widget, div.google, button, script");
     }
 }

commit 182f8e0c58dc1f48e3c864ee2b1744b23de979df

Fix parsers broken by previous commit
Commit: f3c44f4c2ba6776d4e36dfe0c66894561fda3586

plugin/js/parsers/AnythingNovelParser.js
@@ -36,7 +36,7 @@ class AnythingNovelParser extends Parser{
     }
 
     getInformationEpubItemChildNodes(dom) {
-        return [...dom.querySelector("div#content div:not(.clearfix)")];
+        return [...dom.querySelectorAll("div#content div:not(.clearfix)")];
     }
 
     cleanInformationNode(node) {

plugin/js/parsers/FanFictionParser.js
@@ -57,7 +57,7 @@ class FanFictionParser extends Parser {
     }
 
     getInformationEpubItemChildNodes(dom) {
-        return [...dom.querySelector("div#profile_top")];
+        return [...dom.querySelectorAll("div#profile_top")];
     }
 
     cleanInformationNode(node) {